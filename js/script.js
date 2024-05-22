41dfc14d6dd54408c7c13ddac3478166c1569ab8
Stefano Casonato <121874632+khr0me@users.noreply.github.com>
2024-05-22 20:22:20 +0200

    importazione file sito

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function ciao() {
+    alert("that's me, khr0me!");
+}
+
+function transition() {
+    let element = document.getElementById('index');
+
+    element.style.transition = 'all 0.4s'
+}
