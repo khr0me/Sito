4394eb81ed46218c45786ef6e8eac1197f2259de
Stefano Casonato <121874632+khr0me@users.noreply.github.com>
2024-05-27 16:21:55 +0200

    rebuild

--- js/script.js
+++ js/script.js
@@ -2,8 +2,26 @@ function ciao() {
     alert("that's me, khr0me!");
 }
 
-function transition() {
-    let element = document.getElementById('index');
+const landingCarousel = document.querySelector(".slider");
 
-    element.style.transition = 'all 0.4s'
-}
+var count = 0;
+var segment1, segment2;
+segment1 = document.querySelector(".cont")
+const segmentTemplate = segment1.cloneNode(true)
+segment2 = segmentTemplate
+landingCarousel.appendChild(segment2)
+setInterval(() => {
+        landingCarousel.style.left = -count + "px";
+        count += 0.5
+        if (landingCarousel.getBoundingClientRect().x < -(segment1.getBoundingClientRect().width + 30)) {
+            let newSegment = segment2;
+            segment1 = newSegment;
+            segment1.setAttribute("SEGMENT_TYPE", "segment1")
+            segment2 = segmentTemplate;
+            landingCarousel.appendChild(segment2)
+            segment2.setAttribute("SEGMENT_TYPE", "segment2")
+            count = 0;
+        }
+    },
+    1
+)
